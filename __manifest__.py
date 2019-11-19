2c4e229afdc682b6c039b3e80afd296d3ff6ab89
geraQual <56448775+GeraQualsys@users.noreply.github.com>
2019-11-19 15:55:01 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -3,9 +3,14 @@
     'version': '1.2',
     'category': 'Uncategorized',
     'sequence': 60,
-    'summary': '',
-    'description': "primeros cambios en la version 13 de odoo",
-    'website': 'https://www.odoo.com/page/purchase',
+    'summary': '''Cambios realizados en los módulos de 
+    Ventas: Se agrego la cuenta analítica como un campo requerido.
+    Compras: Se agrego un la cuenta analítica como un campo requeido.
+    Campo de fecha de validez agregado en las solicitudes de compras y fecha de vencimiento de la cotización de compra
+    
+    ''',
+    'description': "Modificaciones en los Modelos de Ventas y Compras",
+    'website': 'https://www.qualsys.com,mx',
     'depends': ['purchase', 'sale_management'],
     'data': [
         #'security/purchase_security.xml',
